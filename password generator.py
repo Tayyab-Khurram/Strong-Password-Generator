8be59fce89988d4bd04601940f0d269d6c1eb10c
Tayyab Khurram <tayyabsheikh.pakistan@gmail.com>
2024-10-26 17:14:20 +0500

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -8,7 +8,12 @@ D = string.punctuation
 
 combined = C + B + A + D
 
-plen = int(input('Enter the length of the password : '))
+try:
+    plen = int(input('Enter the length of the password : '))
+except:
+    print('Please enter an integer value only!')
+    plen = int(input('Enter the length of the password : '))
+    
 passwd_list = random.sample(combined, plen)
 passwd = "".join(passwd_list)
 
